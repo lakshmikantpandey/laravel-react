c7e64c18dc4709018ddea0ccfb565ce11ecb6ab4
kutumb_app <lakshmikantpandey02@gmail.com>
2025-01-10 15:01:54 +0530

    initial commit as file uploaded

---
+++ frontend-react/src/App.js
@@ -0,0 +1,49 @@
+import React, { Suspense, useEffect } from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { CSpinner, useColorModes } from '@coreui/react';
+import './scss/style.scss';
+import './scss/examples.scss'; 
+
+const DefaultLayout = React.lazy(() => import('./layout/DefaultLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/pages/login/Login'));
+
+const App = () => {
+  const { isColorModeSet, setColorMode } = useColorModes('coreui-free-react-admin-template-theme');
+  const storedTheme = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
+    const theme = urlParams.get('theme') && urlParams.get('theme').match(/^[A-Za-z0-9\s]+/)[0];
+    if (theme) {
+      setColorMode(theme);
+    }
+
+    if (isColorModeSet()) {
+      return;
+    }
+
+    setColorMode(storedTheme);
+  }, []);
+
+  return (
+    <HashRouter>
+      <Suspense
+        fallback={
+          <div className="pt-3 text-center">
+            <CSpinner color="primary" variant="grow" />
+          </div>
+        }
+      >
+        <Routes>
+          <Route exact path="/login" name="Login Page" element={<Login />} />
+          <Route exact path="*" name="Dashboard" element={<DefaultLayout />} />
+        </Routes>
+      </Suspense>
+    </HashRouter>
+  );
+};
+
+export default App;
